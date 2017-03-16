8954e14cf200e9df2b4ed523a14d45b4b807f707
Emad Zowail <emad.zowail@gmail.com>
2017-03-16 03:07:05 +0200

    Project Files

---
+++ PROJECT_ANAHERA/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace PROJECT_ANAHERA
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.connect = new System.Windows.Forms.Button();
+            this.Send = new System.Windows.Forms.Button();
+            this.dataLog = new System.Windows.Forms.ListBox();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // connect
+            // 
+            this.connect.BackColor = System.Drawing.Color.DarkOrange;
+            this.connect.FlatAppearance.BorderSize = 0;
+            this.connect.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.connect.Font = new System.Drawing.Font("Calibri", 16F);
+            this.connect.ForeColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.connect.Location = new System.Drawing.Point(390, 59);
+            this.connect.Name = "connect";
+            this.connect.Size = new System.Drawing.Size(198, 39);
+            this.connect.TabIndex = 0;
+            this.connect.Text = "Connect";
+            this.connect.UseVisualStyleBackColor = false;
+            this.connect.Click += new System.EventHandler(this.connect_Click);
+            // 
+            // Send
+            // 
+            this.Send.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.Send.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.Send.FlatAppearance.BorderSize = 0;
+            this.Send.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.Send.Font = new System.Drawing.Font("Calibri", 16F);
+            this.Send.ForeColor = System.Drawing.Color.White;
+            this.Send.Location = new System.Drawing.Point(390, 109);
+            this.Send.Name = "Send";
+            this.Send.Size = new System.Drawing.Size(102, 38);
+            this.Send.TabIndex = 1;
+            this.Send.Text = "Send";
+            this.Send.UseVisualStyleBackColor = false;
+            this.Send.Click += new System.EventHandler(this.Send_Click);
+            // 
+            // dataLog
+            // 
+            this.dataLog.FormattingEnabled = true;
+            this.dataLog.Location = new System.Drawing.Point(390, 175);
+            this.dataLog.Name = "dataLog";
+            this.dataLog.Size = new System.Drawing.Size(198, 56);
+            this.dataLog.TabIndex = 3;
+            // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(498, 109);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(90, 38);
+            this.textBox1.TabIndex = 7;
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.SteelBlue;
+            this.button1.FlatAppearance.BorderSize = 0;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(12, 246);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(112, 30);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "Test String";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(130, 246);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(114, 30);
+            this.textBox2.TabIndex = 10;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Transparent;
+            this.label1.Font = new System.Drawing.Font("Calibri", 14.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(387, 152);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 23);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Data received";
+            // 
+            // listBox1
+            // 
+            this.listBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 25;
+            this.listBox1.Location = new System.Drawing.Point(12, 27);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(372, 204);
+            this.listBox1.TabIndex = 16;
+            this.listBox1.SelectedIndexChanged += new System.EventHandler(this.listBox1_SelectedIndexChanged);
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox3.Enabled = false;
+            this.checkBox3.Location = new System.Drawing.Point(390, 292);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(136, 17);
+            this.checkBox3.TabIndex = 19;
+            this.checkBox3.Text = "clear / not scanned yet";
+            this.checkBox3.UseVisualStyleBackColor = false;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox2.Checked = true;
+            this.checkBox2.CheckState = System.Windows.Forms.CheckState.Indeterminate;
+            this.checkBox2.Enabled = false;
+            this.checkBox2.Location = new System.Drawing.Point(390, 269);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(79, 17);
+            this.checkBox2.TabIndex = 18;
+            this.checkBox2.Text = "Down mine";
+            this.checkBox2.UseVisualStyleBackColor = false;
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.BackColor = System.Drawing.Color.Transparent;
+            this.checkBox1.Checked = true;
+            this.checkBox1.CheckState = System.Windows.Forms.CheckState.Checked;
+            this.checkBox1.Enabled = false;
+            this.checkBox1.Location = new System.Drawing.Point(390, 246);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(66, 17);
+            this.checkBox1.TabIndex = 17;
+            this.checkBox1.Text = "UP mine";
+            this.checkBox1.UseVisualStyleBackColor = false;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(390, 27);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(71, 21);
+            this.comboBox1.TabIndex = 20;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BackColor = System.Drawing.Color.Transparent;
+            this.label2.Font = new System.Drawing.Font("Calibri", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(465, 27);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 19);
+            this.label2.TabIndex = 21;
+            this.label2.Text = "Select COM port";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.White;
+            this.BackgroundImage = global::PROJECT_ANAHERA.Properties.Resources.Logo;
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.ClientSize = new System.Drawing.Size(1188, 511);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.dataLog);
+            this.Controls.Add(this.Send);
+            this.Controls.Add(this.connect);
+            this.DoubleBuffered = true;
+            this.Name = "Form1";
+            this.ShowIcon = false;
+            this.Text = "ASU Robotics Team";
+            this.FormClosed += new System.Windows.Forms.FormClosedEventHandler(this.Form1_FormClosed);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button connect;
+        private System.Windows.Forms.Button Send;
+        private System.Windows.Forms.ListBox dataLog;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label2;
+    }
+}
+
---
+++ PROJECT_ANAHERA/Form1.cs
@@ -0,0 +1,425 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PROJECT_ANAHERA
+{
+    public partial class Form1 : Form
+    {
+        string buf;
+        Mine[,] cb = new Mine[19, 19]; //creat a 6x6 check boxs
+        CheckBox[,] cbz = new CheckBox[19, 19]; //creat a 6x6 check boxs
+        string thePort = "";
+        int mx = 0; int my = 0;
+        PictureBox ArrowR = new PictureBox
+        {
+            Name = "pictureBox",
+            Size = new Size(65, 65),
+            Location = new Point(260, 450)
+        };
+
+        PictureBox ArrowL = new PictureBox
+        {
+            Name = "pictureBox",
+            Size = new Size(65, 65),
+            Location = new Point(100, 450)
+        };
+
+        PictureBox ArrowF = new PictureBox
+        {
+            Name = "pictureBox",
+            Size = new Size(65, 65),
+            Location = new Point(180, 400)
+        };
+
+        PictureBox ArrowB = new PictureBox
+        {
+            Name = "pictureBox",
+            Size = new Size(65, 65),
+            Location = new Point(180, 500)
+        };
+
+        PictureBox A = new PictureBox
+        {
+            Name = "pictureBox",
+            Size = new Size(65, 65),
+            Location = new Point(350, 400)
+        };
+
+        PictureBox D = new PictureBox
+        {
+            Name = "pictureBox",
+            Size = new Size(65, 65),
+            Location = new Point(350, 500)
+        };
+
+        public void Default_Image()
+        {
+            ArrowR.ImageLocation = @"C:\Users\ahmed\Desktop\New folder\PROJECT_ANAHERA\Resources\ArrowR.jpg";     
+            ArrowL.ImageLocation = @"C:\Users\ahmed\Desktop\New folder\PROJECT_ANAHERA\Resources\ArrowL.jpg"; 
+            ArrowF.ImageLocation = @"C:\Users\ahmed\Desktop\New folder\PROJECT_ANAHERA\Resources\ArrowF.jpg";      
+            ArrowB.ImageLocation = @"C:\Users\ahmed\Desktop\New folder\PROJECT_ANAHERA\Resources\ArrowB.jpg";
+            A.ImageLocation = @"C:\Users\ahmed\Desktop\New folder\PROJECT_ANAHERA\Resources\ArrowF.jpg";
+            D.ImageLocation = @"C:\Users\ahmed\Desktop\New folder\PROJECT_ANAHERA\Resources\ArrowB.jpg";
+        }
+
+
+        public Form1()
+        {
+            this.MaximumSize = new System.Drawing.Size(1204, 650);
+
+           
+            this.Controls.Add(ArrowR);
+            ArrowR.ImageLocation = @"C:\Users\ahmed\Desktop\New folder\PROJECT_ANAHERA\Resources\ArrowR.jpg";
+           
+      
+            this.Controls.Add(ArrowL);
+            ArrowL.ImageLocation = @"C:\Users\ahmed\Desktop\New folder\PROJECT_ANAHERA\Resources\ArrowL.jpg";
+        
+         
+            this.Controls.Add(ArrowF);
+            ArrowF.ImageLocation = @"C:\Users\ahmed\Desktop\New folder\PROJECT_ANAHERA\Resources\ArrowF.jpg";
+
+            
+            this.Controls.Add(ArrowB);
+            ArrowB.ImageLocation = @"C:\Users\ahmed\Desktop\New folder\PROJECT_ANAHERA\Resources\ArrowB.jpg";
+
+            this.Controls.Add(A);
+            A.ImageLocation = @"C:\Users\ahmed\Desktop\New folder\PROJECT_ANAHERA\Resources\ArrowF.jpg";
+
+            this.Controls.Add(D);
+            D.ImageLocation = @"C:\Users\ahmed\Desktop\New folder\PROJECT_ANAHERA\Resources\ArrowB.jpg";
+
+            buf = "";
+            InitializeComponent();
+            creatCheck(); //to creat an array of check boxs
+            creatCheckz();
+        }
+        void creatCheck()
+        {
+            for (int x = 0; x < 19; x++)
+            {
+
+                for (int y = 0; y < 19; y++)
+                {
+                    Mine box = new Mine(x, y); 
+                    cb[x, y] = box;
+                }
+            }
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            //  tableLayoutPanel1.
+            Size = new Size(1204, 780);
+            foreach (string comPorts in System.IO.Ports.SerialPort.GetPortNames())
+            {
+                comboBox1.Items.Add(comPorts);
+
+            }
+        }
+        private void creatCheckz()
+        {
+            int dy = 400;
+            for (int x = 0; x < 19; x++)
+            {
+                int dx = 620;
+                dy -= 20;
+                for (int y = 0; y < 19; y++)
+                {
+                    CheckBox box;
+                    box = new CheckBox();
+                    box.AutoSize = true;
+                    box.Location = new Point(dx, dy);
+                    box.CheckState = CheckState.Unchecked;
+                    cbz[y, x] = box;
+                    this.Controls.Add(box);
+                    dx += 30;
+                }
+            }
+        }
+        private void connect_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                connect.Text = "Connect";
+                connect.BackColor = Color.FromArgb(128, 255, 128);
+                serialPort1.Close();
+            }
+            else
+            {
+                connect.Text = "End";
+                connect.BackColor = Color.Red;
+            }
+            if (!backgroundWorker1.IsBusy)
+                backgroundWorker1.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            try
+            {
+                serialPort1.PortName = thePort;//comboBox1.SelectedItem.ToString(); //editable
+
+                    serialPort1.Open();                
+            }
+            catch (Exception z)
+            {
+
+                MessageBox.Show(z.ToString());
+            }
+            while (serialPort1.IsOpen)
+            {
+                if (serialPort1.ReadBufferSize > 0)
+                {
+                    try
+                    {
+                        buf = serialPort1.ReadLine();
+                        serialPort1.DiscardInBuffer();
+                    }
+                    catch 
+                    {
+                        
+                       
+                    }
+
+                    //dataLog.Items.Add(serialPort1.ReadLine());
+                   // serialPort1.DiscardInBuffer();
+                }
+
+            }
+
+        }
+
+        private void Send_Click(object sender, EventArgs e)
+        {
+            if (serialPort1.IsOpen)
+            {
+                
+                //serialPort1.WriteLine("u" + trackBar1.Value.ToString());
+                serialPort1.WriteLine(textBox1.Text);
+            }
+            else
+            {
+                MessageBox.Show("DEBUG ERROR");
+            }
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            if (buf != "")
+            {
+                dataLog.Items.Add(buf);
+                try
+                {
+                    showData(buf);
+                }
+                catch
+                {
+                } 
+                buf = "";
+                dataLog.SelectedIndex = dataLog.Items.Count - 1;
+            }
+        }
+
+        private void Form1_FormClosed(object sender, FormClosedEventArgs e)
+        {
+            serialPort1.Close();
+        }
+
+        
+
+
+
+
+
+
+
+
+        double[] da = new double[3]; // a global variable where all the data are stored such that the GUI can read/write to or from it
+        void decodeData(string toDecode)
+        {
+            try
+            {
+                da = Array.ConvertAll(toDecode.Split('-'), double.Parse);
+            }
+            catch
+            {
+                // do nothing :D
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            showData(textBox2.Text);
+        }
+        private void showData(string data)
+        {
+            decodeData(data);
+            mx = (int)da[0];
+            my = (int)(da[1]);
+            var state = da[2].ToString();
+            cb[mx, my].setState(state);
+            cbz[mx, my].CheckState = (state == "1" ? CheckState.Checked: cbz[mx, my].CheckState);
+            cbz[mx, my].CheckState = (state == "2" ? CheckState.Indeterminate : cbz[mx, my].CheckState);
+
+            update();
+        }
+        private void update()
+        {
+            listBox1.Items.Clear();
+            for (int x = 0; x < 19; x++)
+            {
+                for (int y = 0; y < 19; y++)
+                {
+                    if (cb[x, y].state!="clear")
+                    {
+                        listBox1.Items.Add("Mine at : " + x.ToString() + "," + y.ToString() + " state : " + cb[x, y].state );
+
+                    }
+                }
+            }
+        }
+
+        private void tableLayoutPanel1_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            //serialPort1.PortName = "COM9"; //editable
+            //MessageBox.Show( comboBox1.SelectedItem.ToString());
+            thePort = comboBox1.SelectedItem.ToString();
+        }
+
+        private void listBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }   
+
+        protected override bool ProcessCmdKey(ref Message msg, Keys keyData)
+        {
+            //capture up arrow key
+            Default_Image();
+            if (keyData == Keys.Up)
+            {
+                ArrowF.ImageLocation = @"C:\Users\ahmed\Desktop\New folder\PROJECT_ANAHERA\Resources\ArrowFR.jpg";
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.WriteLine("F");
+                }
+              //  Default_Image();
+                return true;
+            }
+            Default_Image();
+            //capture down arrow key       
+            if (keyData == Keys.Down)
+            {  
+               ArrowB.ImageLocation = @"C:\Users\ahmed\Desktop\New folder\PROJECT_ANAHERA\Resources\ArrowBR.jpg";
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.WriteLine("B");   
+                }
+               // Default_Image();
+                return true;
+            }
+           Default_Image();
+            //capture left arrow key
+            if (keyData == Keys.Left)
+            {
+                ArrowL.ImageLocation = @"C:\Users\ahmed\Desktop\New folder\PROJECT_ANAHERA\Resources\ArrowLR.jpg";
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.WriteLine("L");
+                }
+                
+            //  Default_Image();
+                return true;
+            }
+           Default_Image();
+            //capture right arrow key
+            if (keyData == Keys.Right)
+            {
+                ArrowR.ImageLocation = @"C:\Users\ahmed\Desktop\New folder\PROJECT_ANAHERA\Resources\ArrowRR.jpg";
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.WriteLine("R");
+                }
+               // Default_Image();
+                return true;
+            }
+
+           Default_Image();
+            if (keyData == Keys.A)
+            {
+                A.ImageLocation = @"C:\Users\ahmed\Desktop\New folder\PROJECT_ANAHERA\Resources\ArrowFR.jpg";
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.WriteLine("U");
+                }
+              //  Default_Image();
+                return true;
+            }
+            Default_Image();
+            if (keyData == Keys.S)
+            {
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.WriteLine("S");
+                }
+               // Default_Image();
+                return true;
+            }
+            Default_Image();
+            if (keyData == Keys.D)
+            {
+                D.ImageLocation = @"C:\Users\ahmed\Desktop\New folder\PROJECT_ANAHERA\Resources\ArrowBR.jpg";
+                if (serialPort1.IsOpen)
+                {
+                    serialPort1.WriteLine("D");
+                }
+              //  Default_Image();
+                return true;
+            }
+           
+            return base.ProcessCmdKey(ref msg, keyData);
+        }
+
+      
+       
+    }
+    class Mine
+    {
+        int x, y;
+        public string state="clear";
+        public Mine(int x,int y)
+        {
+            this.x = x;
+            this.y = y;
+        }
+        public void setState(string x)
+        {
+            switch (x)
+            {
+                case "1":
+                    state = "surface";
+                    break;
+                case "2":
+                    state = "buried";
+                    break;
+                default:
+                   state = state == "clear" ? x:state;
+                    break;
+            }
+        }
+    }
+
+}
